1f406337996a54a3d8401e5b53818dc52ef5be4e
Mohan Singh <7355184966mohan@gmail.com>
2026-02-12 23:08:23 +0530

    learnt how to use cargo to crate project and run files and include cargo in folder using cargo init

---
+++ matrix/target/debug/deps/matrix.d
@@ -0,0 +1,5 @@
+C:\Users\73551\Desktop\rust\matrix\target\debug\deps\matrix.d: src\main.rs
+
+C:\Users\73551\Desktop\rust\matrix\target\debug\deps\matrix.exe: src\main.rs
+
+src\main.rs:
---
+++ matrix/target/debug/matrix.d
@@ -0,0 +1,1 @@
+C:\Users\73551\Desktop\rust\matrix\target\debug\matrix.exe: C:\Users\73551\Desktop\rust\matrix\src\main.rs
