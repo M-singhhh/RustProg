5ffbf0e840a0608d0b822df06f0db5ffc2aa2e09
Mohan Singh <7355184966mohan@gmail.com>
2026-02-22 07:55:31 +0530

    commit

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,31 +1,52 @@
 use std::io;
 
-fn main() {
-    let mut nums = Vec::new();
-    nums.push(5);
+fn two_sum(nums: &[i32] , target : &i32 ) {
+    let mut boo = false ; 
+    for i in 0 .. nums.len() {
+        for j in i .. nums.len()-1 {
+            if nums[j] + nums[i] == *target {
+                println!("indeces of sum : {} {}",i , j );
+                boo = true ; 
 
-
-    let mut input = String::new();
-
-    // for i in 0..{
-    //     nums.push(i);
-    // }
-    let mut input = String::new();
-    for line in io::stdin().lines(){
-        let line = match line {
-            Ok(l) => l,
-            Err(_) => break, // Exit loop on IO error
-        };
-        match line.trim().parse::<i32>() {
-            Ok(number) => {
-                nums.push(number);
             }
-
+        }
     }
-    
-    
-    
-    
-    println!("{:?}", nums);
+    if !boo {println!("not found ");} 
 }
+
+fn main() {
+    let mut nums = vec![76];
+    println!("enter the number of elements in the array :");
+    let mut inputNO = 35 ;
+    let mut input1 = String::new();
+    io::stdin().read_line(&mut input1 ).expect("failed !!");
+    inputNO = input1.trim().parse().unwrap();
+    for i in 0 .. inputNO {
+        let mut input2 = String::new() ; 
+        io::stdin().read_line(&mut input2 ).expect("failed !!");
+        let mut ioToVec = input2.trim().parse().unwrap();
+        nums.push(ioToVec);
+    }
+
+    
+    let mut index = -1 ; 
+    println!("enter the target number :"); 
+    let mut target = 35 ;
+    let mut input = String::new();
+    io::stdin().read_line(&mut input ).expect("failed !!");
+    target = input.trim().parse().unwrap();
+
+
+    
+    
+    
+    
+    two_sum(&nums , &target);
+    
+    
+    
+    
+    
+    
+    
 }
